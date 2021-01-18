683fa94ca4c37d201c8b0509b710b3599a9acf37
Jessica Deacon <j.r.deacon@hotmail.co.uk>
2021-01-18 17:03:40 +0000

    add components

--- src/pages/productPage.js
+++ src/pages/productPage.js
@@ -1,2 +1,14 @@
 import React from "react";
 import styled from "styled-components";
+import { Link, useHistory } from "react-router-dom";
+import { navBarTheme } from "./themes";
+import { ThemeProvider } from "styled-components";
+import { Container } from "./components/Forms";
+
+const Product = () => {
+  return (
+    <Container>
+      <h1>Product</h1>
+    </Container>
+  );
+};
