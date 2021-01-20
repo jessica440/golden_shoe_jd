a669bbb6345ea07ab8dd6f7aa65889acb920ffe2
Jessica Deacon <j.r.deacon@hotmail.co.uk>
2021-01-20 16:26:15 +0000

    start api request

--- src/api/api.js
+++ src/api/api.js
@@ -1,3 +1,16 @@
+function request(path, options) {
+  return fetch(url + path, options).then((response) => {
+    if (!response.ok && response.status !== 409) {
+      const error = new Error("Error!");
+      error.status = response.status;
+      throw error;
+    } else {
+      console.log(response);
+      return response.json();
+    }
+  });
+}
+
 function returns(orderNumber, quantity, reason) {
   return request("user", {
     method: "POST",
