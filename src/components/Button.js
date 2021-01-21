9f6a409e1ce4d5407e128be0c27d53121e283e4b
Jessica Deacon <j.r.deacon@hotmail.co.uk>
2021-01-21 17:53:32 +0000

    add alert pop-up

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,2 +1,40 @@
 import React from "react";
 import styled from "styled-components";
+
+const Button = styled.button`
+  font-size: 1rem;
+  font-weight: bold;
+`;
+
+export const Content = styled.div`
+  font-size: 2rem;
+  /* display: none; */
+`;
+
+// export const CollapsibleText = () => {
+//   const collapse = () => {
+//     console.log("click");
+//     if (Content.display === "none") {
+//       Content.display = "block";
+//     } else {
+//       Content.display = "none";
+//     }
+//   };
+
+export const CollapsibleText = () => {
+  const [seeText, setSeeText] = React.useState(false);
+  const handleSeeText = () => {
+    setSeeText(!seeText);
+  };
+
+  return (
+    <>
+      <Button onClick={() => handleSeeText()}>Button</Button>
+      <Content>
+        <h1>Hello!</h1>
+      </Content>
+    </>
+  );
+};
+
+export default CollapsibleText;
