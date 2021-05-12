36b7f07b22e21cf90cca5fac36064e3b735f8e67
Jessica Deacon <j.r.deacon@hotmail.co.uk>
2021-05-12 11:39:24 +0100

    style

--- src/pages/productPage.js
+++ src/pages/productPage.js
@@ -1,19 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "./../components/NavBar";
 import { shoes } from "../data/shoeData";
 import { Shoe, ShoeGrid, ShoeLabel } from "../components/ShoeGrid";
 
 export const Products = () => {
+  const [shoeInfo, setShoeInfo] = useState();
   return (
     <>
       <NavBar />
       {/* <StyledMenu> */}
       {Object.entries(shoes).map(([sectionName, shoes]) => (
         <>
+          <p>{shoeInfo}</p>
           <h1>{sectionName}</h1>
           <ShoeGrid>
             {shoes.map((shoes) => (
-              <Shoe img={shoes.img}>{shoes.name}</Shoe>
+              <Shoe
+                img={shoes.img}
+                onClick={() => {
+                  setShoeInfo(shoes.name);
+                }}
+              >
+                {shoes.name}
+              </Shoe>
             ))}
             <ShoeLabel>{shoes.price}</ShoeLabel>
           </ShoeGrid>
--- src/pages/returnsDeliveryPage.js
+++ src/pages/returnsDeliveryPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import NavBar from "./../components/NavBar";
 import { Container } from "./../components/Form";
-import { CollapsibleText, Content } from "./../components/Button";
+import { CollapsibleText } from "./../components/Button";
 
 const DeliveryReturns = () => {
   return (
