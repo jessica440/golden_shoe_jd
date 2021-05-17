e7b6065062a91d30cee08e412e911d59c5b8881c
Jessica Deacon <j.r.deacon@hotmail.co.uk>
2021-05-17 14:10:03 +0100

    style forms

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,12 +1,24 @@
 import React from "react";
+import styled from "styled-components";
+
 import NavBar from "./../components/NavBar";
 import { Container } from "./../components/Form";
 import HomeImage from "./../components/HomeImage";
+import Trainers from "../icons/trainersBackground.jpg";
+
+export const Img = styled.img`
+  width: 100vw;
+  opacity: 0.5;
+  position: absolute;
+  z-index: -1;
+`;
 
 const HomePage = () => {
   return (
     <>
       <NavBar />
+      <Img scr={Trainers} />
+
       <HomeImage />
       <Container></Container>
     </>
